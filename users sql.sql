35bfac5be9d74589d3dcb928aa59f3fd3ba130fc
lahari-cyber <lahariakireddy@gmail.com>
2026-07-21 20:51:43 +0530

    Updated assignment documents and removed users sql.sql

--- users sql.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users(
-id INT,
-username VARCHAR(100) UNIQUE,
-email VARCHAR(100) UNIQUE
-);
-select * from users;
-Insert users (id,username,email)  values(1,"Lahari","lahari@gmail.com"),
-(2,"hi",NULL);
